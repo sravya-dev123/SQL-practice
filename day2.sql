e79e32965e1345fe423d8571305d887e86674e0b
sravya sangadi <sravyasangadi@gmail.com>
2026-08-31 08:56:35 +0530

    WHERE clause
    Practiced basic SQL commands including WHERE clause 
    The WHERE clause is used to extract only those records that fulfill a specific condition.
    syntax: SELECT*FROM sravya
                WHERE marks=66;

---
+++ day2.sql
@@ -0,0 +1,24 @@
+
+/*create database : its used to create a big container of storing data into databse*/
+CREATE DATABASE sravya;
+use sravya;
+/*create table: its used to cerate a table by using instructions on it*/
+CREATE TABLE sravya(
+rollno INT(5),
+name VARCHAR(20),
+branch VARCHAR(30),
+marks INT(5),
+grade VARCHAR(3)
+);
+/* insert commands used to insert or adding the values*/
+INSERT INTO sravya VALUES
+(101,'priya','CSE',66,'C'),
+(102,'sravya','ECE',96,'A+'),
+(103,'sneha','AI',66,'C'),
+(104,'ramya','CSE',82,'B+'),
+(105,'bhavya','MECH',76,'B');
+/*select command : its used to SELECT for retrieving data using MySQL. */
+SELECT*FROM sravya;
+
+SELECT*FROM sravya
+WHERE marks=66;
